14378c66ee9797ae6f9da4dfb6d6239746a27b32
unironically <lbvac230@gmail.com>
2022-09-20 14:34:27 -0500

    Added back declarations and references.

--- grammars/scopetree/Scope.sv
+++ grammars/scopetree/Scope.sv
@@ -2,12 +2,16 @@ grammar scopetree;
 
 nonterminal Graph<d r> with root_scopes<d r>;
 nonterminal Scope<d r> with id, str, parent<d r>, children<d r>;
+nonterminal Decl<d r> with str, name, line, column, in_scope<d r>, assoc_scope<d r>;
+nonterminal Ref<d r> with str, name, line, column, in_scope<d r>;
 
 synthesized attribute id::Integer;
 synthesized attribute str::String;
 synthesized attribute parent<d r>::Maybe<Decorated Scope<d r>>;
 synthesized attribute children<d r>::[Decorated Scope<d r>];
 synthesized attribute root_scopes<d r>::[Decorated Scope<d r>];
+synthesized attribute in_scope<d r>::Decorated Scope<d r>;
+synthesized attribute assoc_scope<d r>::Maybe<Decorated Scope<d r>>;
 
 synthesized attribute name::String;
 synthesized attribute line::Integer;
@@ -53,3 +57,49 @@ top::Scope<d r> ::=
 
 --------------------
 -- Declaration nodes
+
+abstract production mk_decl
+  attribute name i occurs on d,
+  attribute line i occurs on d,
+  attribute column i occurs on d =>
+top::Decl<d r> ::= 
+  ast_node::Decorated d with i
+  in_scope::Decorated Scope<d r>
+{
+  forwards to mk_decl_assoc(ast_node, in_scope, nothing());
+}
+
+abstract production mk_decl_assoc
+  attribute name i occurs on d,
+  attribute line i occurs on d,
+  attribute column i occurs on d =>
+top::Decl<d r> ::= 
+  ast_node::Decorated d with i
+  in_scope::Decorated Scope<d r>
+  assoc_scope::Maybe<Decorated Scope<d r>>
+{
+  top.str = top.name ++ "_" ++ toString(top.line) ++ "_" ++ toString(top.column);
+  top.name = ast_node.name;
+  top.line = ast_node.line;
+  top.column = ast_node.column;
+  top.in_scope = in_scope;
+  top.assoc_scope = assoc_scope;
+}
+
+--------------------
+-- Reference nodes
+
+abstract production mk_ref
+  attribute name i occurs on r,
+  attribute line i occurs on r,
+  attribute column i occurs on r =>
+top::Ref<d r> ::=
+  ast_node::Decorated r with i
+  in_scope::Decorated Scope<d r>
+{
+  top.str = top.name ++ "_" ++ toString(top.line) ++ "_" ++ toString(top.column);
+  top.name = ast_node.name;
+  top.line = ast_node.line;
+  top.column = ast_node.column;
+  top.in_scope = in_scope;
+}
