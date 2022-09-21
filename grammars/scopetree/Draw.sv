60c62f3c32fefe4d208e21ca5138fa74d9b24000
unironically <lbvac230@gmail.com>
2022-09-21 13:21:11 -0500

    More work on scope graph drawing.

--- grammars/scopetree/Draw.sv
+++ grammars/scopetree/Draw.sv
@@ -7,17 +7,17 @@ String ::= graph::Graph<d r>
 {
   return "digraph {" ++ 
 
-      "{ node [shape=circle style=solid fontsize=" ++ graphviz_font_size ++ "] " ++
+      "\n{node [shape=circle style=solid fontsize=" ++ graphviz_font_size ++ "] " ++
       graphviz_scope_labels(graph.root_scopes) ++ "}" ++ 
 
-      "{ node [shape=box fontsize=" ++ graphviz_font_size ++  "] edge [arrowhead=normal] " ++ 
+      "\n{node [shape=box fontsize=" ++ graphviz_font_size ++  "] edge [arrowhead=normal]\n" ++ 
       foldl((\acc::String cur::Decorated Scope<d r> -> 
-        acc ++ " " ++ graphviz_draw_declrefs(cur)), "", graph.root_scopes) ++ 
-      "}" ++
+        acc ++ graphviz_draw_declrefs(cur)), "", graph.root_scopes) ++ 
+      "}\n" ++
 
       graphviz_parent_edges(graph.root_scopes) ++ 
     
-    "}";
+    "\n}";
 }
 
 function graphviz_scope_labels
@@ -34,10 +34,11 @@ function graphviz_draw_declrefs
 String ::= scope::Decorated Scope<d r>
 {
   return 
-    "\n" ++ foldl ( -- decls
+    "/* Scope " ++ scope.str ++ " */" ++ foldl ( -- decls
       (\acc::String cur::Decorated Decl<d r> ->
         acc ++ " " ++ scope.str ++ "->" ++ cur.str ++
-        (case cur.assoc_scope of nothing() -> "" | just(s) -> "{ edge [arrowhead=onormal]" ++ cur.str ++ "->" ++ s.str ++ "}" end)),
+        case cur.assoc_scope of nothing() -> "" | just(s) -> 
+          "{edge [arrowhead=onormal]" ++ cur.str ++ "->" ++ s.str ++ "}" end),
       "",
       scope.decls
     ) ++ "" ++
@@ -47,18 +48,18 @@ String ::= scope::Decorated Scope<d r>
         acc ++ " " ++ cur.str ++ "->" ++ scope.str),
       "",
       scope.refs
-    ) ++ "" ++
+    ) ++ " " ++
 
     "{edge [arrowhead=onormal]" ++ foldl( -- imports
       (\acc::String cur::Decorated Ref<d r> -> 
         acc ++ " " ++ scope.str ++ "->" ++ cur.str),
       "",
       scope.imps
-    ) ++ "}" ++
+    ) ++ "}\n" ++
 
     foldl ( -- continuing to child scopes
       (\acc::String cur::Decorated Scope<d r> -> 
-        acc ++ " " ++ graphviz_draw_declrefs(cur)), 
+        acc ++ graphviz_draw_declrefs(cur)), 
       "", 
       scope.children
     );
@@ -68,8 +69,8 @@ function graphviz_parent_edges
 String ::= scope_list::[Decorated Scope<d r>]
 {
   return foldl(
-    (\acc::String scope::Decorated Scope<d r> -> acc ++ " " ++ 
-      case scope.parent of nothing() -> "" | just(p) -> scope.str ++ " -> " ++ p.str end ++ 
+    (\acc::String scope::Decorated Scope<d r> -> acc ++ 
+      case scope.parent of nothing() -> "" | just(p) -> scope.str ++ "->" ++ p.str ++ " " end ++
       graphviz_parent_edges(scope.children)),
     "",
     scope_list
