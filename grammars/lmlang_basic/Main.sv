11e434f33235b71e3bd92a5359ac5110aeae2e0d
unironically <lbvac230@gmail.com>
2022-09-22 20:55:18 -0500

    Added new grammar lmlang_basic - with aspect productions will implement (id,dcl) assoc list of name bindings.

---
+++ grammars/lmlang_basic/Main.sv
@@ -0,0 +1,26 @@
+grammar lmlang_basic;
+
+imports lmlang as lm;
+
+parser parse :: lm:Program_c {
+    lmlang;
+}
+
+function main
+IO<Integer> ::= largs::[String]
+{
+  local attribute args::String;
+  args = head(largs);
+
+  local attribute result :: ParseResult<lm:Program_c>;
+  result = parse(args, "<<args>>");
+
+  local attribute r_cst::lm:Program_c;
+  r_cst = result.parseTree;
+
+  local attribute r::lm:Program = r_cst.lm:ast;
+
+  return if result.parseSuccess
+    then do {print("Success!\n" ++ r.lm:pp ++ "\n"); return 0;}
+    else do {print("Something went wrong!\n"); return -1;};
+}
---
+++ scripts/compile-lmlang_basic.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#silver -I ../grammars $@ old-stlc-wip
+
+mkdir -p ../bin
+
+silver -I ../grammars --onejar $@ lmlang_basic
+
+rm build.xml > /dev/null 2>&1
+rm Parser_lmlang_basic_parse.copperdump.html > /dev/null 2>&1
+
+mv *.jar ../bin > /dev/null 2>&1
