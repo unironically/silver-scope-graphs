259cf00bdbfd4a1ff7207ac8d8837230d5b0a30d
unironically <lbvac230@gmail.com>
2023-02-16 17:01:12 -0600

    Altered first test file.

--- grammars/scopegraph/Main.sv
+++ grammars/scopegraph/Main.sv
@@ -8,18 +8,21 @@ function main
 IO<Integer> ::= largs::[String]
 {
   return do {
-    let fileName :: String = head(largs);
+    let filePath :: String = head(largs);
     file :: String <- readFile(head(largs));
+
+    let fileNameExt :: String = last(explode("/", filePath));
+    let fileName :: String = head(explode(".", fileNameExt));
     
-    let result :: ParseResult<Program_c> = parse (file, fileName);
+    let result :: ParseResult<Program_c> = parse (file, filePath);
     let r_cst :: Program_c = result.parseTree;
     let sg :: Decorated Graph = r_cst.graph_c;
     
-    graphviz_printed :: Integer <- 
-      system ("echo '" ++ graphviz_draw_graph(sg) ++ "' | dot -Tsvg > scope_graph.svg");
+    let drawn :: String = graphviz_draw_graph(sg);
+    system ("echo '" ++ drawn ++ "' | dot -Tsvg > sg_" ++ fileName ++ ".svg");
     
     if result.parseSuccess
-      then do {print ("Success!\n"); return 0;}
+      then do {print ("Success: see sg_" ++ fileName ++ ".svg\n"); return 0;}
       else do {print ("Failure!\n"); return -1;};
   };
 }
