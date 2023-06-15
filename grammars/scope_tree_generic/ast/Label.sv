76fa348dc9fb9e9b8a95fdfb04ba7687f6ba9ff1
unironically <lbvac230@gmail.com>
2023-06-15 16:29:05 -0500

    Beginning an outline for generic scope tree library

--- grammars/scope_tree_generic/ast/Label.sv
+++ grammars/scope_tree_generic/ast/Label.sv
@@ -1,3 +1,44 @@
 grammar scope_tree_generic:ast;
 
 nonterminal Label;
+
+{-
+
+The productions for labels are defined in the object language specification, and
+each production for the Label nonterminal pertains to a particular label type,
+such as `PAR, `DEC, `REC, etc. 
+
+The ordering of edge labels should also be encoded within the Label nonterminal,
+perhaps the tree of Label produtions structurally encodes the '>' relation on
+labels within a language specification?
+
+-}
+
+
+nonterminal Regex;
+
+{-
+
+Something here to encode the regular expressions that are assigned to queries in
+the language specifications.
+
+A `next` attribute to signify which edge labels we are allowed to follow for
+the next transition?
+
+-}
+
+abstract production concatenate
+top::Regex ::= r1::Regex r2::Regex
+{}
+
+abstract production star
+top::Regex ::= r1::Regex
+{}
+
+abstract production alternate
+top::Regex ::= r1::Regex r2::Regex
+{}
+
+abstract production single
+top::Regex ::= label::Label
+{}
--- grammars/scope_tree_generic/ast/Scope.sv
+++ grammars/scope_tree_generic/ast/Scope.sv
@@ -1,3 +1,33 @@
 grammar scope_tree_generic:ast;
 
 nonterminal Scope;
+
+abstract production mk_scope_generic
+top::Scope ::= label::Integer datum::Maybe<(Identifier, Datum)>
+{}
+
+abstract production mk_scope
+top::Scope ::= label::Integer
+{ forwards to mk_scope_generic (label, nothing()); }
+
+abstract production mk_scope_datum
+top::Scope ::= label::Integer datum::(Identifier, Datum)
+{ forwards to mk_scope_generic (label, just (datum)); }
+
+
+nonterminal Datum;
+
+{-
+
+The productions for Datum should be defined in the object language.
+
+-}
+
+
+nonterminal Identifier;
+
+{-
+
+The productions for Identifier should be defined in the object language.
+
+-}
