b05fe30351c2eb1be6fc772507897b6f5da41d0d
unironically <lbvac230@gmail.com>
2023-06-19 23:49:07 -0500

    Yet another dysfunctional regex to nfa impl.

--- grammars/scope_tree_generic/ast/Label.sv
+++ grammars/scope_tree_generic/ast/Label.sv
@@ -53,7 +53,7 @@ Boolean ::= l1::Label l2::Label
     | (imp_prod (_), imp_prod (_)) -> true
     | (mod_prod (_), mod_prod (_)) -> true
     | (rec_prod (_), rec_prod (_)) -> true
-    | _                        -> false
+    | _                            -> false
     end;
 }
 
@@ -61,6 +61,15 @@ Boolean ::= l1::Label l2::Label
 
 nonterminal Regex;
 synthesized attribute nfa :: NFA occurs on Regex;
+synthesized attribute final_nfa :: Decorated NFA occurs on Regex;
+
+abstract production regex
+top::Regex ::= r1::Regex
+{
+  local my_nfa :: NFA = nfa (r1.nfa);
+  my_nfa.used_id = 0;
+  top.final_nfa = my_nfa;
+}
 
 abstract production concatenate
 top::Regex ::= r1::Regex r2::Regex
@@ -88,138 +97,185 @@ top::Regex ::= label::Label
 
 {- NFAs -}
 
-
 nonterminal NFA;
-synthesized attribute start :: NFA_State occurs on NFA;
-synthesized attribute accepting :: [NFA_State] occurs on NFA;
+synthesized attribute start       :: Integer occurs on NFA;
+synthesized attribute accepting   :: Integer occurs on NFA;
+synthesized attribute transitions :: [(Integer, Maybe<Label>, Integer)] occurs on NFA;
+inherited attribute used_id :: Integer occurs on NFA;
+synthesized attribute took_id :: Integer occurs on NFA;
+
+abstract production nfa
+top::NFA ::= n1::NFA
+{
+  n1.used_id = 0;
+  top.start = n1.start;
+  top.accepting = n1.accepting;
+  top.transitions = n1.transitions;
+  top.took_id = n1.took_id;
+}
 
 abstract production nfa_concatenate
 top::NFA ::= n1::NFA n2::NFA
 {
-  top.start = n1.start.epsilon_combine (n1.accepting, n2.start);
+  top.start = n1.start;
   top.accepting = n2.accepting;
+  top.transitions = 
+    (n1.accepting, nothing (), n2.start) :: n1.transitions ++ n2.transitions;
+  n1.used_id = top.used_id;
+  n2.used_id = n1.took_id;
+  top.took_id = n2.took_id;
 }
-
 
 abstract production nfa_alternate
 top::NFA ::= n1::NFA n2::NFA
 {
-  local state1 :: NFA_State = mk_nfa_state ([n1.start, n2.start], [], [], [], [], [], []);
-  local state2 :: NFA_State = mk_nfa_state ([], [], [], [], [], [], []);
-
-  top.start = state1.epsilon_combine (n1.accepting ++ n2.accepting, state2);
-  top.accepting = [state2];
+  local state1 :: Integer = top.used_id + 1;
+  local state2 :: Integer = top.used_id + 2;
+  top.start = state1;
+  top.accepting = state2;
+  top.transitions = 
+    (top.start, nothing (), n1.start) ::
+    (top.start, nothing (), n2.start) ::
+    (n1.accepting, nothing (), top.accepting) ::
+    (n2.accepting, nothing (), top.accepting) ::
+    n1.transitions ++
+    n2.transitions;
+  
+  n1.used_id = top.used_id + 2;
+  n2.used_id = n1.took_id;
+  top.took_id = n2.took_id;
 }
-
 
 abstract production nfa_star
 top::NFA ::= n1::NFA
 {
-  local state1 :: NFA_State = mk_nfa_state ([n1.start, state2], [], [], [], [], [], []);
-  local state2 :: NFA_State = mk_nfa_state ([], [], [], [], [], [], []);
-
-  top.start = (state1.epsilon_combine (n1.accepting, n1.start)).epsilon_combine (n1.accepting, state2);
-  top.accepting = [state2];
+  local state1 :: Integer = top.used_id + 1;
+  local state2 :: Integer = top.used_id + 2;
+  top.start = state1;
+  top.accepting = state2;
+  top.transitions = 
+    (top.start, nothing (), n1.start) ::
+    (top.start, nothing (), top.accepting) ::
+    (n1.accepting, nothing (), n1.start) ::
+    (n1.accepting, nothing (), top.accepting) ::
+    n1.transitions;
+  n1.used_id = top.used_id + 2;
+  top.took_id = n1.took_id;
 }
-
 
 abstract production nfa_single
 top::NFA ::= label::Label
 {
-  local state1 :: NFA_State =
-    case label of
-      mod_prod (_) -> mk_nfa_state ([], [state2], [], [], [], [], [])
-    | var_prod (_) -> mk_nfa_state ([], [], [state2], [], [], [], [])
-    | rec_prod (_) -> mk_nfa_state ([], [], [], [state2], [], [], [])
-    | ext_prod (_) -> mk_nfa_state ([], [], [], [], [state2], [], [])
-    | imp_prod (_) -> mk_nfa_state ([], [], [], [], [], [state2], [])
-    | lex_prod (_) -> mk_nfa_state ([], [], [], [], [], [], [state2])
-    end;
-  local state2 :: NFA_State = mk_nfa_state ([], [], [], [], [], [], []);
-
+  local state1 :: Integer = top.used_id + 1;
+  local state2 :: Integer = top.used_id + 2;
   top.start = state1;
-  top.accepting = [state2];
+  top.accepting = state2;
+  top.transitions =
+    (top.start, just (label), top.accepting) :: [];
+  top.took_id = top.used_id + 2;
 }
 
 
-{- NFA states -}
 
-nonterminal NFA_State;
-synthesized attribute id::String occurs on NFA_State;
-synthesized attribute epsilon_combine :: (NFA_State ::= [NFA_State] NFA_State) occurs on NFA_State;
-synthesized attribute next :: ([NFA_State] ::= Label) occurs on NFA_State;
-
-abstract production mk_nfa_state
-top::NFA_State ::= 
-  eps_trans :: [NFA_State]
-  mod_trans :: [NFA_State]
-  var_trans :: [NFA_State]
-  rec_trans :: [NFA_State]
-  ext_trans :: [NFA_State]
-  imp_trans :: [NFA_State]
-  lex_trans :: [NFA_State]
+function accepts
+Boolean ::= nfa::Decorated NFA labs::[Label]
 {
+  local result :: [Integer] = steps (nfa, nfa.start, labs);
 
-  top.id = toString (genInt ());
-
-  top.epsilon_combine = \ acc::[NFA_State] s::NFA_State ->
-    epsilon_combine_f (acc, s, top);
-
-  top.next = \ l::Label ->
-    case l of
-      mod_prod (_) -> concat (map (grab_all_eps, mod_trans))
-    | var_prod (_) -> concat (map (grab_all_eps, var_trans))
-    | rec_prod (_) -> concat (map (grab_all_eps, rec_trans))
-    | ext_prod (_) -> concat (map (grab_all_eps, ext_trans))
-    | imp_prod (_) -> concat (map (grab_all_eps, imp_trans))
-    | lex_prod (_) -> concat (map (grab_all_eps, lex_trans))
-    end
-    ++ concat (map ((\s::NFA_State -> s.next (l)), eps_trans));
-
+  return unsafeTrace (containsBy (state_eq (_, _, nfa), nfa.accepting, result), printT (
+    "Got to: " ++ implode (" ", map ((\x::Integer -> toString (x)), result)) ++ "\n"
+    , unsafeIO()));
 }
 
-function grab_all_eps
-[NFA_State] ::= s::NFA_State
+function steps
+[Integer] ::= nfa::Decorated NFA current::Integer labs::[Label]
 {
   return
-    case s of
-      mk_nfa_state (es, _, _, _, _, _, _) ->
-        es ++ concat (map (grab_all_eps, es))
+    case labs of
+      [] -> [current]
+    | l::ls -> 
+        let 
+          next :: [Integer] = step (nfa, current, l)
+        in
+          concat (map (steps (nfa, _, ls), next))
+        end
     end;
 }
 
-function epsilon_combine_f
-NFA_State ::= acc::[NFA_State] to_state::NFA_State current_state::NFA_State
+function step
+[Integer] ::= nfa::Decorated NFA s::Integer lab::Label
+{
+
+
+  local valid_steps :: [(Integer, Maybe<Label>, Integer)] =
+    filter (\pair :: (Integer, Maybe<Label>, Integer) -> 
+              state_eq (s, fst(pair), nfa) && valid_lab (lab, fst (snd (pair))), 
+            nfa.transitions);
+  
+
+  local part :: ([(Integer, Maybe<Label>, Integer)], [(Integer, Maybe<Label>, Integer)]) =
+    partition (is_eps_trans, unsafeTrace (valid_steps, printT ("Valid len: " ++ toString (length (valid_steps)) ++ "\n", unsafeIO())));
+  
+
+  local eps_trans :: [(Integer, Maybe<Label>, Integer)] = 
+    unsafeTrace (fst (part), printT("Eps trans -> " ++ implode (" ", map ((\x::(Integer, Maybe<Label>, Integer) -> toString(fst(x))), fst(part))) ++ "\n", unsafeIO()));
+  local eps_to :: [Integer] = 
+    map ((\tup::(Integer, Maybe<Label>, Integer) -> snd (snd (tup))), eps_trans);
+
+
+  local lab_trans :: [(Integer, Maybe<Label>, Integer)] = 
+    unsafeTrace (snd (part), printT("Valid trans -> " ++ implode (" ", map ((\x::(Integer, Maybe<Label>, Integer) -> toString( snd(snd(x)) )), snd(part))) ++ "\n", unsafeIO()));
+
+  local lab_to :: [Integer] = 
+    map ((\tup::(Integer, Maybe<Label>, Integer) -> snd (snd (tup))), lab_trans);
+
+
+  local eps_done :: [Integer] = 
+    concat (map (step (nfa, _, lab), eps_to));
+
+
+  return unsafeTrace (eps_done ++ lab_to, printT ("Returning: " ++ implode (" ", map (toString, eps_done ++ lab_to)) ++ "\n", unsafeIO ()));
+
+}
+
+
+
+
+function valid_lab
+Boolean ::= want::Label has::Maybe<Label>
 {
   return
-    case current_state of
-      mk_nfa_state (eps, mod, var, rec, ext, imp, lex) ->
-        mk_nfa_state (
-          let 
-            new_eps :: [NFA_State] = epsilon_combine_map (acc, to_state, eps)
-          in 
-            if containsBy (state_eq, current_state, acc) 
-              then to_state :: new_eps 
-              else new_eps 
-          end,
-          epsilon_combine_map (acc, to_state, mod),
-          epsilon_combine_map (acc, to_state, var),
-          epsilon_combine_map (acc, to_state, rec),
-          epsilon_combine_map (acc, to_state, ext),
-          epsilon_combine_map (acc, to_state, imp),
-          epsilon_combine_map (acc, to_state, lex)
-        )
+    case (want, has) of
+      (_, nothing ()) -> true
+    | (lex_prod (_), just(lex_prod (_))) -> true
+    | (ext_prod (_), just(ext_prod (_))) -> true
+    | (var_prod (_), just(var_prod (_))) -> true
+    | (imp_prod (_), just(imp_prod (_))) -> true
+    | (mod_prod (_), just(mod_prod (_))) -> true
+    | (rec_prod (_), just(rec_prod (_))) -> true
+    | _                                  -> false
     end;
 }
 
-function epsilon_combine_map
-[NFA_State] ::= acc::[NFA_State] to_state::NFA_State old::[NFA_State]
+function is_eps_trans
+Boolean ::= trans::(Integer, Maybe<Label>, Integer)
 {
-  return map (epsilon_combine_f (acc, to_state, _), old);
+  return
+    case trans of
+      (_, nothing (), _) -> true
+    | _ -> false
+    end;
 }
+
+
+{- Utils -}
 
 function state_eq
-Boolean ::= s1::NFA_State s2::NFA_State
+Boolean ::= s1::Integer s2::Integer nfa::Decorated NFA
 { 
-  return s1.id == s2.id; 
+  return unsafeTrace (s1 == s2, printT ("Does " ++ toString(s1) ++ " equal " ++ toString(s2) ++ " ? " ++ toString (s1 == s2) ++ "\n" ++
+  "\t Valid transitions: " ++ implode ("\n\t", map ((\pair::(Integer, Maybe<Label>, Integer) -> "(" ++ toString(fst (pair)) ++ ", " ++ toString(snd (snd (pair))) ++ ")"), nfa.transitions)) ++ "\n"
+  
+  
+  , unsafeIO())); 
 }
--- grammars/scope_tree_generic/driver/Driver.sv
+++ grammars/scope_tree_generic/driver/Driver.sv
@@ -11,15 +11,11 @@ IO<Integer> ::= largs::[String]
     
     print ("Test!\n");
 
-    let regex :: Regex = star (single (lex_lab));
+    let regex :: Regex = regex (star (single (lex_lab)));
 
-    let start :: NFA_State = regex.nfa.start;
-    print ("start: " ++ start.id ++ "\n");
-    print ("accepting: " ++ implode (" ", map ((\s::NFA_State -> s.id), regex.nfa.accepting)) ++ "\n");
-    print ("--------------\n");
-
-    let one_lex :: [NFA_State] = start.next (lex_lab);
-    print (foldl ((\str::String s::NFA_State -> str ++ s.id ++ "\n"), "", one_lex));
+    if accepts (regex.final_nfa, [])
+      then print ("Accepts!\n")
+      else print ("Rejects!\n");
 
     return 0;
 
